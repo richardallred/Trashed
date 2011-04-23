1a12b8386f8137b76961414adb50c7e2aacff397
heelsfan23r <rmallred@email.unc.edu>
2011-04-23 10:55:07 -0700

    Edited src/WaveGen.java via GitHub

--- src/WaveGen.java
+++ src/WaveGen.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import javax.swing.ImageIcon;
 
 import javax.swing.*;
 
@@ -9,8 +10,9 @@ public class WaveGen {
 	public WaveGen(Board aBoard){
 		theBoard=aBoard;
 	}
+
     private ArrayList<Trash> wave= new ArrayList<Trash>();
- 
+
     
     public ArrayList<Trash> getWave(int level){
     	int num =0;
@@ -18,39 +20,22 @@ public class WaveGen {
     	int initY= Util.pathPad;
     	int speed= 0;
     	ArrayList<Util.TrashType> types = new ArrayList<Util.TrashType>();
-    	
     	if (level ==1){
     	num =10;
     	spacing = 135;
-    	speed =2;     //high for testing purposes, should be 2
+    	speed =2;
     	types.add(Util.TrashType.paper);
-    	types.add(Util.TrashType.aluminum);
-    	types.add(Util.TrashType.metal);
-   
+    	
+    		
     	}else if (level ==2){
-   
+    	
+    	
     	num =20;
     	spacing = 85;
     	speed =3;
     	types.add(Util.TrashType.paper);
     	
-    	
-    	}else if (level ==3){
-    		num = 25;
-    		spacing = 85;
-    		speed =3;
-    		types.add(Util.TrashType.paper);
-    		types.add(Util.TrashType.plastic);
-    	}else if (level ==4){
-    		num = 30;
-    		spacing= 80;
-    		speed = 4;
-    		types.add(Util.TrashType.plastic);
-    		types.add(Util.TrashType.aluminum);
     	}
-    	
-    	
-    	
     	int curX=-30;
     	
     	for(int i=0; i<num; i++){
@@ -64,18 +49,13 @@ public class WaveGen {
     	
     }
 
-public ArrayList<ImageIcon> getMessages(int level) {
+    public ArrayList<ImageIcon> getMessages(int level) {
 		ArrayList<ImageIcon> temp = new ArrayList<ImageIcon>();
 		// JJrandomShit
 		if (level == 1)
-			temp.add(new ImageIcon(this.getClass().getResource(
-					"pics/welcome1.png")));
+			temp.add(new ImageIcon(this.getClass().getResource("pics/welcome1.png")));
 		if (level == 2)
-			temp.add(new ImageIcon(this.getClass().getResource(
-					"pics/welcome3.png")));
-		if (level == 3)
-			temp.add(new ImageIcon(this.getClass().getResource(
-					"pics/welcome2.png")));
+			temp.add(new ImageIcon(this.getClass().getResource("pics/welcome2.png")));
 		return temp;
 	}
 }
