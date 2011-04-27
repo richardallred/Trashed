24275374d474615688fde186aa4884e8806d1b56
rmallred <rmallred@192.168.1.148>
2011-04-27 00:36:09 -0400

    added fun factz!

--- src/Menu.java
+++ src/Menu.java
@@ -3,6 +3,9 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.Random;
+
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -28,8 +31,12 @@ public class Menu extends JPanel implements Runnable {
 	private String currentTowerDirection = "South";
 	private JLabel info = new JLabel("");
 	private JLabel towerInfo = new JLabel("");
+	private JLabel fact = new JLabel("");
 	private JButton sellButton,cancelButton,muteButton,effectMuteButton,upgradeButton;
 	private Tower clickedTower;
+	private ArrayList<String> facts= new ArrayList<String>();
+	Random generator = new Random();
+	
 
 	public Menu(Board board) {
 		setDoubleBuffered(true);
@@ -71,7 +78,9 @@ public class Menu extends JPanel implements Runnable {
 		
 		JButton startWaveButton = new JButton("<html><center>Send Next Wave</center></html>");
 		startWaveButton.addActionListener(new StartWaveButtonListener());
-		startWaveButton.setBounds(0, 450, 300, 50);
+		startWaveButton.setBounds(0, 400, 300, 50);
+		
+		fact.setBounds(5, 450, 290, 100);
 		
 		muteButton = new JButton("<html><center>Mute Music</center></html>");
 		muteButton.addActionListener(new MuteButtonListener());
@@ -92,6 +101,46 @@ public class Menu extends JPanel implements Runnable {
 		add(startWaveButton);
 		add(muteButton);
 		add(effectMuteButton);
+		add(fact);
+		
+		
+		//Set up facts array
+		
+		facts.add("<html><center>To produce each week's Sunday newspapers, 500,000 trees must be cut down.</center></html>");
+		facts.add("<html><center>Recycling a single run of the Sunday <i>New York Times</i> would save 75,000 trees.</center></html>");
+		facts.add("<html><center>If all our newspaper was recycled, we could save about 250,000,000 trees each year!</center></html>");
+		facts.add("<html><center>If every American recycled just one-tenth of their newspapers, we would save about 25,000,000 trees a year.</center></html>");
+		facts.add("<html><center>If you had a 15-year-old tree and made it into paper grocery bags, you'd get about 700 of them. A busy supermarket could use all of them in under an hour! This means in one year, one supermarket can go through over 6 million paper bags! Imagine how many supermarkets there are just in the United States!!!</center></html>"); 
+		facts.add("<html><center>The average American uses seven trees a year in paper, wood, and other products made from trees. This amounts to about 2,000,000,000 trees per year!</center></html>");
+		facts.add("<html><center>The amount of wood and paper we throw away each year is enough to heat 50,000,000 homes for 20 years. </center></html>");
+		facts.add("<html><center>Approximately 1 billion trees worth of paper are thrown away every year in the U.S.</center></html>");
+		facts.add("<html><center>Americans use 85,000,000 tons of paper a year; about 680 pounds per person.</center></html>");
+		facts.add("<html><center>The average household throws away 13,000 separate pieces of paper each year. Most is packaging and junk mail.</center></html>");
+		facts.add("<html><center>In 1993, U.S. paper recovery saved more than 90,000,000 cubic yards of landfill space.</center></html>");
+		facts.add("<html><center>Each ton (2000 pounds) of recycled paper can save 17 trees, 380 gallons of oil, three cubic yards of landfill space, 4000 kilowatts of energy, and 7000 gallons of water. This represents a 64% energy savings, a 58% water savings, and 60 pounds less of air pollution!</center></html>");
+		facts.add("<html><center>The 17 trees saved (above) can absorb a total of 250 pounds of carbon dioxide from the air each year. Burning that same ton of paper would <i>create</i> 1500 pounds of carbon dioxide.</center></html>"); 
+		facts.add("<html><center>The construction costs of a paper mill designed to use waste paper is 50 to 80% less than the cost of a mill using new pulp.</center></html>"); 
+		facts.add("<html><center>Americans use 2,500,000 plastic bottles every hour! Most of them are thrown away!</center></html>");
+		facts.add("<html><center>Plastic bags and other plastic garbage thrown into the ocean kill as many as 1,000,000 sea creatures every year!</center></html>");
+		facts.add("<html><center>Recycling plastic saves twice as much energy as burning it in an incinerator.</center></html>"); 
+		facts.add("<html><center>Americans throw away 25,000,000,000 Styrofoam coffee cups every year.</center></html>"); 
+		facts.add("<html><center>Every month, we throw out enough glass bottles and jars to fill up a giant skyscraper. All of these jars are recyclable!</center></html>");
+		facts.add("<html><center>The energy saved from recycling one glass bottle can run a 100-watt light bulb for four hours or a compact fluorescent bulb for 20 hours. It also causes 20% less air pollution and 50% less water pollution than when a new bottle is made from raw materials.</center></html>");
+		facts.add("<html><center>A modern glass bottle would take 4000 years or more to decompose -- and even longer if it's in the landfill.</center></html>");
+		facts.add("<html><center>Mining and transporting raw materials for glass produces about 385 pounds of waste for every ton of glass that is made. If recycled glass is substituted for half of the raw materials, the waste is cut by more than 80%.</center></html>"); 
+		facts.add("<html><center>About one-third of an average dump is made up of packaging material!</center></html>");
+		facts.add("<html><center>Every year, each American throws out about 1,200 pounds of organic garbage that can be composted.</center></html>");
+		facts.add("<html><center>The U.S. is the #1 trash-producing country in the world at 1,609 pounds per person per year. This means that 5% of the world's people generate 40% of the world's waste.</center></html>");
+		facts.add("<html><center>The highest point in Hamilton County, Ohio (near Cincinnati) is 'Mount Rumpke'. It is actually a mountain of trash at the Rumpke sanitary landfill towering 1045 ft. above sea level.</center></html>");
+		facts.add("<html><center>The US population discards each year 16,000,000,000 diapers, 1,600,000,000 pens, 2,000,000,000 razor blades, 220,000,000 car tires, and enough aluminum to rebuild the US commercial air fleet four times over.</center></html>"); 
+		facts.add("<html><center>Out of every $10 spent buying things, $1 (10%) goes for packaging that is thrown away. Packaging represents about 65% of household trash.</center></html>");
+		facts.add("<html><center>On average, it costs $30 per ton to recycle trash, $50 to send it to the landfill, and $65 to $75 to incinerate it.</center></html>");
+		facts.add("<html><center>An estimated 80,000,000 Hershey's Kisses are wrapped each day, using enough aluminum foil to cover over 50 acres of space -- that's almost 40 football fields. All that foil is recyclable, but not many people realize it.</center></html>");
+		facts.add("<html><center>Rainforests are being cut down at the rate of 100 acres per minute!</center></html>");
+		facts.add("<html><center>A single quart of motor oil, if disposed of improperly, can contaminate up to 2,000,000 gallons of fresh water.</center></html>");
+		facts.add("<html><center>Motor oil never wears out, it just gets dirty. Oil can be recycled, re-refined and used again, reducing our reliance on imported oil.</center></html>");
+		facts.add("<html><center>On average, each one of us produces 4.4 pounds of solid waste each day. This adds up to almost a ton of trash per person, per year.</center></html>");
+		facts.add("<html><center>A typical family consumes 182 gallons of soda, 29 gallons of juice, 104 gallons of milk, and 26 gallons of bottled water a year. That's a lot of containers -- make sure they're recycled!</center></html>");
 
 	}
 
@@ -573,9 +622,17 @@ private class UpgradeTowerButtonListener implements ActionListener {
 		long beforeTime, timeDiff, sleep;
 
 		beforeTime = System.currentTimeMillis();
+		int curLevel=0;
+		int oldLevel=0;
 
 		while (true) {
-
+			curLevel=gameBoard.getLevel();
+			if(curLevel>oldLevel){
+				fact.setText(getFact());
+				fact.repaint();
+				oldLevel=curLevel;
+			}
+			
 			long pause = 0;
 			if(clickedTower!=null){
 				setTowerInfoText(clickedTower);
@@ -619,6 +676,18 @@ private class UpgradeTowerButtonListener implements ActionListener {
 		super.addNotify();
 		menu = new Thread(this);
 		menu.start();
+	}
+	
+	private String getFact(){
+		
+		
+		int rand=generator.nextInt(facts.size());
+		
+		System.out.println(rand);
+		
+		return facts.get(rand);
+		
+		
 	}
 
 }
